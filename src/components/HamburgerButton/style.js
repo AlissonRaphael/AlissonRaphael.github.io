647e68a3201459353c9c5c1a328d1300b338a079
Alisson Oliveira <alissonraphaeloliveira@gmail.com>
2021-07-17 16:39:51 -0300

    refactor: update HamburgerButton component

--- src/components/HamburgerButton/style.js
+++ src/components/HamburgerButton/style.js
@@ -3,19 +3,20 @@ import { motion } from 'framer-motion'
 
 export const ButtonMenu = styled(motion.div)`
 
-width: 28px;
-height: 28px;
-cursor: pointer;
-position: relative;
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+  position: relative;
+  z-index: 100;
 
-div {
-  position: absolute;
-  top: 0;
-  left: 0;
+  div {
+    position: absolute;
+    top: 0;
+    left: 0;
 
-  svg {
-    width: 28px;
-    height: 28px;
+    svg {
+      width: 28px;
+      height: 28px;
+    }
   }
-}
 `
